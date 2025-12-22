4b4332e9dd00ad7213c0ae118d676a4b36bf39a1
Burak Istanbullu <155552983+burakistanbullu@users.noreply.github.com>
2025-12-22 19:59:45 +0300

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull burakistanbullu/simple-python-flask-app:
+docker pull burakistanbullu/simple-python-flask-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 burakistanbullu/simple-python-flask-app
